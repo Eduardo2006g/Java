82543a8f8a2f33df74858761f50d4f35b0090a4d
Eduardo2006g <101937977+Eduardo2006g@users.noreply.github.com>
2022-09-22 22:25:27 -0300

    Update botoes4.js

--- botoes4.js
+++ botoes4.js
@@ -1,18 +1,45 @@
+let titulo
+let tamanho = 6;
 
-        let tamanho = 3;
-        function atualizatamanho() {
-            const h1 = document.querySelector("h1");
-            h1.style.fontSize = `${tamanho}em`;
+for(let i = 1; i < 7; i++) {
+    titulo = document.querySelector(`h${i}`)
+    if(titulo != null){
+        criabotao()
+        break
+    }
+}
+
+function criabotao() {
+    const botoes = document.createElement('div')
+
+    const btn = document.createElement('button')
+    btn.innerText = "+"
+    btn.id = 'btn1'
+    
+    const btn2 = document.createElement('button')
+    btn2.innerText = "-"
+    btn2.id = 'btn2'
+
+    botoes.append(btn)
+    botoes.append(btn2)
+
+    titulo.parentNode.insertBefore(botoes, titulo.nextSibling)
+    event()
+}
+function event(){
+    const botao1 = document.querySelector('#btn1')
+    botao1.addEventListener('click', ()=>{
+        if(tamanho <= 8){
+            tamanho += 1
         }
-        function aumentatitulo() {
-            if (tamanho < 6) {
-                tamanho += 0.5;
-                atualizatamanho()
-            }
+        titulo.style.fontSize = `${tamanho}em`
+    })
+    const botao2 = document.querySelector('#btn2')
+    botao2.addEventListener('click',  ()=>{
+        if(tamanho > 1){
+            tamanho -= 1
         }
-        function diminuititulo() {
-            if (tamanho > 1) {
-                tamanho -= 0.5;
-                atualizatamanho();
-            }
-        }
+        titulo.style.fontSize = `${tamanho}em`
+    })
+}
+
